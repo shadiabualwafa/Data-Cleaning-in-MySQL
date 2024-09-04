d21f7847ec73e4c061366d4037f61c503adcc9ab
Shadi Abualwafa <shadi2020abualwafa@outlook.com>
2024-09-04 17:47:23 +0300

    Add One comment

--- MySql/Data Cleaning in MySQL.sql
+++ MySql/Data Cleaning in MySQL.sql
@@ -1,4 +1,8 @@
 
+-- first thing we want to do is create a staging table. 
+-- This is the one we will work in and clean the data.
+-- We want a table with the raw data in case something happens
+
 CREATE TABLE layoffs_staging
 LIKE dayoffs
 ;
